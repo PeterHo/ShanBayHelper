ba038e61630c937c9a441663b1038aeff1077ef7
Peter Ho <peterho1024@gmail.com>
2017-06-13 21:49:21 +0800

    text to html

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 choco.exe install -y PhantomJS
 pip install  -t ./site-packages -r .\requirements.txt
+
+python main.py username password > words.html
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+
+import sys
 from selenium import webdriver
 
 from selenium.webdriver.common.by import By
@@ -7,16 +9,22 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 theEnd = False
+index = 1
 
 def getWordListAndGoToNextPage(driver):
     global theEnd
+    global index
     master = driver.find_element_by_id("master")
     words = master.find_elements_by_class_name("word")
     for word in words:
-        print(word.text)
+        print(str(index) + ". " + word.text + "<br>")
+        index += 1
 
     if theEnd:
         return False
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".jquery-bootstrap-pagination.pagination")))
 
     pagination = driver.find_element_by_css_selector(".jquery-bootstrap-pagination.pagination")
     links = pagination.find_elements_by_tag_name("a")
@@ -31,28 +39,44 @@ def getWordListAndGoToNextPage(driver):
     time.sleep(2)
     return True
 
-def begin():
+
+def begin(username, password):
     # driver = webdriver.PhantomJS()
     driver = webdriver.Firefox()
 
     driver.get("https://www.shanbay.com/web/account/login/")
 
-    username = driver.find_element_by_name("username")
-    password = driver.find_element_by_name("password")
+    usernameInput = driver.find_element_by_name("username")
+    passwordInput = driver.find_element_by_name("password")
 
-    username.send_keys("username")
-    password.send_keys("password")
-    password.send_keys(Keys.RETURN)
+    usernameInput.send_keys(username)
+    passwordInput.send_keys(password)
+    passwordInput.send_keys(Keys.RETURN)
 
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "task-1")))
 
     driver.get("https://www.shanbay.com/bdc/learnings/library/#master_tab")
 
+    print('''
+<html>
+<head>
+<style>
+body{text-align:center}
+</style>
+</head>
+<body>
+    ''')
+
     while getWordListAndGoToNextPage(driver):
         pass
 
+    print('''
+</body>
+</html>
+    ''')
+
     driver.quit()
 
-if __name__ == '__main__':
-    begin()
 
+if __name__ == '__main__':
+    begin(sys.argv[1], sys.argv[2])
